02a06171b4758c6772e3f8e79aafc30f414c307b
muhammad-aldubouni <muhammad.aldubouni.expserv@gmail.com>
2025-05-31 19:37:44 +0300

    removing build directory

--- build/core/libb64/cdecode.c.d
+++
@@ -1,7 +0,0 @@
-/home/hb/files/projects/esp/esp32/home-system/build/core/libb64/cdecode.c.o: \
- /home/hb/.arduino15/packages/esp32/hardware/esp32/1.0.6/cores/esp32/libb64/cdecode.c \
- /home/hb/.arduino15/packages/esp32/hardware/esp32/1.0.6/cores/esp32/libb64/cdecode.h \
- /home/hb/.arduino15/packages/esp32/hardware/esp32/1.0.6/tools/sdk/include/newlib/stdint.h \
- /home/hb/.arduino15/packages/esp32/hardware/esp32/1.0.6/tools/sdk/include/newlib/machine/_default_types.h \
- /home/hb/.arduino15/packages/esp32/hardware/esp32/1.0.6/tools/sdk/include/newlib/sys/features.h \
- /home/hb/.arduino15/packages/esp32/hardware/esp32/1.0.6/tools/sdk/include/newlib/sys/_intsup.h
